3ff6b2a933cc341fef06ccb60307fe7dcecd6392
Stone Pack <stone.pack@icloud.com>
2024-04-02 21:28:39 -0700

    initial work for device auth keychain items

--- ios/Classes/KeymasterPlugin.swift
+++ ios/Classes/KeymasterPlugin.swift
@@ -1,6 +1,7 @@
 import Flutter
-import UIKit
+import LocalAuthentication
 import Security
+import UIKit
 
 public class KeymasterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -14,36 +15,37 @@ public class KeymasterPlugin: NSObject, FlutterPlugin {
       
     switch call.method {
     case "delete":
-        guard let key = arguments["key"] as? String else {
+        guard let key = arguments["key"] as? String, let auth = arguments["auth"] as? Bool else {
             result(nil)
             return
         }
         
-        let success = Keychain.deleteKeychainValue(key: key)
+        let success = Keychain.deleteKeychainValue(key: key, authRequired: auth)
         result(success)
     case "fetch":
-        guard let key = arguments["key"] as? String else {
+        guard let key = arguments["key"] as? String, let auth = arguments["auth"] as? Bool else {
             result(nil)
             return
         }
         
-        let res = Keychain.fetchKeychainValue(key: key)
+        let res = Keychain.fetchKeychainValue(key: key, authRequired: auth)
         result(res)
     case "set":
-        guard let key = arguments["key"] as? String, let data = arguments["value"] as? String else {
+        guard let key = arguments["key"] as? String, let data = arguments["value"] as? String, let auth = arguments["auth"] as? Bool else {
             result(nil)
             return
         }
         
-        let res = Keychain.fetchKeychainValue(key: key)
-        
-        if (res == nil) {
-            let success = Keychain.setKeychainValue(key: key, value: data)
-            result(success)
-        } else {
-            let success = Keychain.updateKeychainValue(key: key, value: data)
-            result(success)
+        let success = Keychain.setKeychainValue(key: key, value: data, requireAuth: auth)
+        result(success)
+    case "update":
+        guard let key = arguments["key"] as? String, let data = arguments["value"] as? String, let auth = arguments["auth"] as? Bool else {
+            result(nil)
+            return
         }
+        
+        let success = Keychain.updateKeychainValue(key: key, value: data, authRequired: auth)
+        result(success)
     default:
       result(FlutterMethodNotImplemented)
     }
@@ -51,14 +53,22 @@ public class KeymasterPlugin: NSObject, FlutterPlugin {
 }
 
 class Keychain {
-    public static func deleteKeychainValue(key: String) -> Bool {
+    public static func deleteKeychainValue(key: String, authRequired: Bool) -> Bool {
         let bundleID: String = Bundle.main.bundleIdentifier ?? "keymaster"
         let account = "\(bundleID).\(key)"
         
-        let query: [String: Any] = [
+        let context = LAContext()
+        context.localizedReason = "Authenticate to update keychain"
+        
+        var query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: account,
+            kSecUseAuthenticationContext as String: context,
         ]
+        
+        if (!authRequired) {
+            query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUISkip
+        }
         
         if SecItemDelete(query as CFDictionary) == noErr {
             return true
@@ -67,17 +77,25 @@ class Keychain {
         return false
     }
     
-    public static func fetchKeychainValue(key: String) -> String? {
+    public static func fetchKeychainValue(key: String, authRequired: Bool) -> String? {
         let bundleID: String = Bundle.main.bundleIdentifier ?? "keymaster"
         let account = "\(bundleID).\(key)"
         
-        let query: [String: Any] = [
+        let context = LAContext()
+        context.localizedReason = "Authenticate to read from keychain"
+        
+        var query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: account,
             kSecMatchLimit as String: kSecMatchLimitOne,
             kSecReturnAttributes as String: true,
             kSecReturnData as String: true,
+            kSecUseAuthenticationContext as String: context,
         ]
+        
+        if (!authRequired) {
+            query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUISkip
+        }
         
         var item: CFTypeRef?
         
@@ -92,17 +110,22 @@ class Keychain {
         return nil
     }
     
-    public static func setKeychainValue(key: String, value: String) -> Bool {
+    public static func setKeychainValue(key: String, value: String, requireAuth: Bool) -> Bool {
         let bundleID: String = Bundle.main.bundleIdentifier ?? "keymaster"
         let account = "\(bundleID).\(key)"
         
         let data = value.data(using: .utf8)!
         
-        let attributes: [String: Any] = [
+        var attributes: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: account,
+            kSecAttrAccessControl as String: access,
             kSecValueData as String: data,
         ]
+        
+        if (requireAuth) {
+            attributes[kSecAttrAccessControl as String] = SecAccessControlCreateWithFlags(nil, kSecAttrAccessibleWhenUnlocked, .userPresence, nil)!
+        }
         
         let res = SecItemAdd(attributes as CFDictionary, nil)
         
@@ -110,16 +133,24 @@ class Keychain {
         return false
     }
     
-    public static func updateKeychainValue(key: String, value: String) -> Bool {
+    public static func updateKeychainValue(key: String, value: String, authRequired: Bool) -> Bool {
         let bundleID: String = Bundle.main.bundleIdentifier ?? "keymaster"
         let account = "\(bundleID).\(key)"
         
         let data = value.data(using: .utf8)!
         
-        let query: [String: Any] = [
+        let context = LAContext()
+        context.localizedReason = "Authenticate to update keychain"
+        
+        var query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrAccount as String: account,
+            kSecUseAuthenticationContext as String: context,
         ]
+        
+        if (!authRequired) {
+            query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUISkip
+        }
         
         let attributes: [String: Any] = [kSecValueData as String: data]
         if SecItemUpdate(query as CFDictionary, attributes as CFDictionary) == noErr {
--- lib/keymaster.dart
+++ lib/keymaster.dart
@@ -3,28 +3,59 @@ import 'package:flutter/services.dart';
 class Keymaster {
   static const MethodChannel _channel = MethodChannel('keymaster');
 
-  static Future<bool?> delete(String key) async {
+  static Future<bool?> delete(String key, {bool? authRequired}) async {
     bool? result = await _channel.invokeMethod(
       'delete',
-      {'key': key},
+      {
+        'key': key,
+        'auth': authRequired ?? false,
+      },
     );
 
     return result;
   }
 
-  static Future<String?> fetch(String key) async {
+  static Future<String?> fetch(String key, {bool? authRequired}) async {
     String? result = await _channel.invokeMethod(
       'fetch',
-      {'key': key},
+      {
+        'key': key,
+        'auth': authRequired ?? false,
+      },
     );
 
     return result;
   }
 
-  static Future<bool?> set(String key, String value) async {
+  static Future<bool?> set(
+    String key,
+    String value, {
+    bool? authRequired,
+  }) async {
     bool? result = await _channel.invokeMethod(
       'set',
-      {'key': key, 'value': value},
+      {
+        'key': key,
+        'value': value,
+        'auth': authRequired ?? false,
+      },
+    );
+
+    return result;
+  }
+
+  static Future<bool?> update(
+    String key,
+    String value, {
+    bool? authRequired,
+  }) async {
+    bool? result = await _channel.invokeMethod(
+      'update',
+      {
+        'key': key,
+        'value': value,
+        'auth': authRequired ?? false,
+      },
     );
 
     return result;
